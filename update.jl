bb7e35b8f97f1b2cd2541a848fb5dad5e6d02974
Damien Gonot <damien.gonot@gmail.com>
2022-02-18 13:56:27 -0500

    format .jl file and switch from md to org-mode file

--- update.jl
+++ update.jl
@@ -11,59 +11,57 @@ end
 
 function format_data(json)
     prices = json["prices"]
-    Dict("x" => map(p->p[1] / 1000, prices), "y" => map(p->p[2], prices))
+    Dict("x" => map(p -> p[1] / 1000, prices), "y" => map(p -> p[2], prices))
 end
 
 function generate_graph(data)
     p = lineplot(
         data["x"],
         data["y"],
-        title = "BTC price last 24 hours",
-        xlabel = "UNIX timestamp",
-        ylabel = "\$ USD",
-        width = 60,
-        canvas = DotCanvas,
-        border = :ascii)
+        title="BTC price last 24 hours",
+        xlabel="UNIX timestamp",
+        ylabel="\$ USD",
+        width=60,
+        canvas=DotCanvas,
+        border=:ascii)
     fn = "graph.txt"
 
     savefig(p, fn)
     read(fn, String)
 end
 
-function format_md(chart)
+function format_org(chart)
     header = """
-# 👋
+* 👋
 
-```
+#+begin_example
 """
     footer = """
 
-```
+#+end_example
 📈 Data provided by CoinGecko
 
 🧑‍💻 I'm Damien
 
-✍️ I blog at [damiengonot.com](https://www.damiengonot.com)
-
-🎨 Code is art, therefore you're an artist
+✍️ I blog at [[https://www.damiengonot.com][damiengonot.com]]
 """
 
-header * chart * footer
+    header * chart * footer
 end
 
-function write_md(md)
-    f = open("README.md", "w")
-    write(f, md)
+function write_org(org)
+    f = open("readme.org", "w")
+    write(f, org)
     close(f)
 end
 
 function git_commit_push()
     run(`rm graph.txt`)
-    run(`git add README.md`)
+    run(`git add readme.org`)
     run(`git commit -m $(Dates.format(Dates.now(), "yyyy-mm-dd HH:MM"))`)
     run(`git push origin main`)
 end
 
 ts = round(Int, time())
-query_data("bitcoin", ts-(60*60*24), ts) |> format_data |> generate_graph |> format_md |> write_md
+query_data("bitcoin", ts - (60 * 60 * 24), ts) |> format_data |> generate_graph |> format_org |> write_org
 git_commit_push()
